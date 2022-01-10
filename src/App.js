7350b42d41e40dc854442c0e9cec6d8f0b1f0b12
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2022-01-10 13:56:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./Screens/HomeScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductScreen from './Screens/ProductScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />}></Route>
+            <Route path="/product/:id" element={<ProductScreen/>}></Route>
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
